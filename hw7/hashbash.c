c489c6027376d538279860f07dab36eea9b22503
mdarafay7 <mdarafay@gmail.com>
2019-04-16 13:40:49 -0500

    pheww

--- hw7/hashbash.c
+++ hw7/hashbash.c
@@ -20,8 +20,35 @@ typedef struct {
 typedef struct {
   int size;
   int count;
-  ht_item items[29908];
+  ht_item** items;
 } ht_hash_table;
+
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->meaning = strdup(v);
+    return i;
+}
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+
+    ht->size =29908;
+    ht->count =29908;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+    return ht;
+}
+
+void strip(char *s) {
+    char *p2 = s;
+    while(*s != '\0') {
+        if(*s != '\t' && *s != '\n') {
+            *p2++ = *s++;
+        } else {
+            ++s;
+        }
+    }
+    *p2 = '\0';
+}
 
  unsigned int hash_bash(char* string)
 {
@@ -42,60 +69,187 @@ unsigned int hash_bash2(char *string,int value)
   return value*sum;
 }
 
- int insert(char *word,char *meaning, ht_item* item,ht_hash_table* collection)
-{
-  int position;
-  position=(hash_bash(word))%29881;
-  int prob=1;
-  while(collection->items[position].key)
-  {
-    position=(hash_bash(word)+hash_bash2(word,prob))%29881;
-    prob++;
-
-  }
-  prob=0;
-  collection->items[position].key=word;
-  collection->items[position].meaning=meaning;
-  //printf("\n%d) %s || %s",prob,collection->items[position].key,collection->items[position].meaning);
-}
-
-int search(char *word,ht_item* item,ht_hash_table* collection)
-{
-  int position;
-  position=(hash_bash(word))%29881;
-  int prob=1;
-  while(strcmp(collection->items[position].key,word)&&(prob<29881&&position<29881))
-  {
-    position=(hash_bash(word)+hash_bash2(word,prob))%29881;
-    if(!strcmp(collection->items[position].key,word))
-    {
-      prob++;
-      break;
-    }
-    prob++;
-
-  }
-  return prob;
-}
-
-int delete()
-
+//  int insert(char *word,char *meaning, ht_item* item,ht_hash_table* collection)
+// {
+//   int position;
+//   int match=0;
+//   position=(hash_bash(word))%29881;
+//   int prob=1;
+//
+//   while(collection->items[position]->key)
+//   {
+//
+//
+//     prob++;
+//     if(collection->items[position]->key!=NULL)
+//     {
+//     if(!strcmp(collection->items[position]->key,word))
+//     {
+//         match=1;
+//     }
+//     }
+//
+//     position=(hash_bash(word)+hash_bash2(word,prob))%29881;
+//
+//
+//   }
+//   if(match)
+//   {
+//     //memset(collection->items[position]->meaning,'\0',1000*sizeof(char));
+//     free(collection->items[position]->meaning);
+//     collection->items[position]->meaning = malloc(1000*sizeof(char));
+//     //strcat(collection->items[position]->meaning,meaning);
+//     printf("\nto be inserted %ld\n",strlen(meaning));
+//   }
+//   else{
+//   collection->items[position]->key = malloc(100*sizeof(char));
+//   collection->items[position]->meaning = malloc(1000*sizeof(char));
+//   strcpy(collection->items[position]->key,word);
+//   strcpy(collection->items[position]->meaning,meaning);
+//   printf("\nto be inserted %ld\n",strlen(meaning));
+//
+//   }
+//
+//  printf("\n inserted pair [%s,%s]\n",collection->items[position]->key,collection->items[position]->meaning);
+// }
+//
+// int search(char *word,ht_item* item,ht_hash_table* collection)
+// {
+//   int position;
+//   position=(hash_bash(word))%29881;
+//   int prob=1;
+//   int result=0;
+//   //printf("hhh %s hhh",collection->items[7856]->key);
+//   fflush(NULL);
+//   fflush(stdin);
+//   fflush(stdout);
+//
+//   if(position>=29881||collection->items[position]->key==NULL)
+//   {
+//     printf("\nNot Found");
+//     fflush(NULL);
+//     return 0;
+//   }
+//
+//   if(!strcmp(collection->items[position]->key,word))
+//   {
+//     printf("\nFound %s || %s || %s ",word,collection->items[position]->key,collection->items[position]->meaning);
+//
+//     result=1;
+//
+//   }
+//
+//
+//
+//   while((strcmp(collection->items[position]->key,word)&&!result)&&(prob<29881&&position<29881))
+//   {
+//     //printf("AAAA");
+//     position=(hash_bash(word)+hash_bash2(word,prob))%29881;
+//     if(!strcmp(collection->items[position]->key,word))
+//     {
+//       prob++;
+//       result=1;
+//       printf("\nFound %s || %s || %s ",word,collection->items[position]->key,collection->items[position]->meaning);
+//
+//       break;
+//     }
+//     prob++;
+//
+//   }
+//   printf("\n%d probes ",prob);
+//   if(!result)
+//   {
+//     printf("\nNot Foundpp\n\n\n");
+//   }
+//   else
+//   {
+//     //printf("\nTranslation %s",collection->items[position]->meaning);
+//   }
+//   return position;
+// }
+//
+// int delete(char *word,ht_item* item,ht_hash_table* collection)
+// {
+//   int position;
+//   position=(hash_bash(word))%29881;
+//   int prob=1;
+//   int result=0;
+//   //printf("hhh %s hhh",collection->items[7856]->key);
+//   fflush(NULL);
+//   fflush(stdin);
+//   fflush(stdout);
+//   if(position>=29881||collection->items[position]->key==NULL)
+//   {
+//     printf("\nNot Found");
+//     fflush(NULL);
+//     return 0;
+//   }
+//
+//   if(!strcmp(collection->items[position]->key,word))
+//   {
+//     free(collection->items[position]->key);
+//     free(collection->items[position]->meaning);
+//     collection->items[position]->key=NULL;
+//     collection->items[position]->meaning=NULL;
+//     result=1;
+//
+//   }
+//
+// if(!result){
+//   while(strcmp(collection->items[position]->key,word)&&(prob<29881&&position<29881))
+//   {
+//     //printf("AAAA");
+//     position=(hash_bash(word)+hash_bash2(word,prob))%29881;
+//     if(!strcmp(collection->items[position]->key,word))
+//     {
+//       prob++;
+//       result=1;
+//       free(collection->items[position]->key);
+//       free(collection->items[position]->meaning);
+//       collection->items[position]->key=NULL;
+//       collection->items[position]->meaning=NULL;
+//
+//       break;
+//     }
+//     prob++;
+//
+//   }
+// }
+//   printf("\n%d probes ",prob);
+//   if(!result)
+//   {
+//     printf("\nNot Foundpp\n\n\n");
+//   }
+//   else
+//   {
+//     printf("\nDeleted %s\n",word);
+//   }
+//   return position;
+// }
+//
 
 
 
 
 int main(int argc, char** argv) {
   {
-  int x=0,prob=0,count=0;
-  int result;
-  unsigned int position,position2;
-  ht_item item;
-  ht_hash_table collection;
-  char word[BUFFERSIZE];
-  char filename[BUFFERSIZE];
-  char word_insert[BUFFERSIZE];
+
+  int length=0;
+  int prob=0,count=0,repeat=0;
+  //int result;
+  unsigned int position=0;
+  //ht_item item;
+  ht_hash_table* collection = ht_new();
+
+
+
+  char word[BUFFERSIZE]="aaa";
+  char filename[BUFFERSIZE]="aaa";
+  int ptr_track[29882];
+  ptr_track[0]=1;
+  //char word_insert[BUFFERSIZE];
   char meaning[BUFFERSIZE];
-  char term[BUFFERSIZE];
+  //char term[BUFFERSIZE];
   word[0]='1';
 scanf("%s",filename);
 //printf("%s %s",word,meaning);
@@ -103,7 +257,7 @@ FILE *fp=fopen(filename,"r");
 printf("%s",filename);
 
 
-int c;
+//int c;
 if(!fp)
 {
   printf("AAAAAAAAAAAAAAA");
@@ -112,51 +266,105 @@ fflush(stdout);
 fflush(NULL);
 fflush(stdin);
 
+//fscanf(fp,"%s",word);
 
-    while(strcmp(word,"Fatiha"))
+    while(fscanf(fp,"%s",word)!=EOF)
     {
-      fscanf(fp,"%s",word);
+      repeat=0;
       fgets(meaning, BUFFERSIZE, fp);
+      strip(meaning);
       position=(hash_bash(word))%29881;
       prob=1;
-      while(collection.items[position].key)
+     while(collection->items[position]->key)
       {
+        // printf("\n%s KK\n",collection->items[position]->key);
+        // printf("\n%s WW\n",word);
+        // fflush(NULL);
+        if(!strcmp(collection->items[position]->key,word)!=NULL)
+        {
+          length = strlen(collection->items[position]->meaning);
+          memset(collection->items[position]->meaning +length, ';',1);
+          collection->items[position]->meaning[length+1] = '\0';
+          strcat(collection->items[position]->meaning,meaning);
+          repeat=1;
+          break;
+        }
         position=(hash_bash(word)+hash_bash2(word,prob))%29881;
         prob++;
 
       }
       prob=0;
-      collection.items[position].key=word;
-      collection.items[position].meaning=meaning;
-      count++;
-    //  printf("\n%d) %s || %s",count,collection.items[position].key,collection.items[position].meaning);
+      if(!repeat)
+      {
+        count++;
+        ptr_track[count]=position;
+      //  collection->items[position]->key=0;
+        //collection->items[position]->meaning=0;
+
+      collection->items[position]->key = (char*)calloc(100,sizeof(char));
+      collection->items[position]->meaning =(char*)calloc(1000,sizeof(char));
+
+      strcpy(collection->items[position]->key,word);
+      //meaning[strlen(meaning)-0]=0;
+      strcpy(collection->items[position]->meaning,meaning);
+
+    }
+
+
+      printf("\n%d )%s || %s",position,collection->items[position]->key, collection->items[position]->meaning);
+      //fscanf(fp,"%s",word);
+      memset(meaning,'\0', sizeof(meaning));
+      memset(word,'\0', sizeof(word));
       }
 
-scanf("%s",term);
 
-while(strcmp(term,"q"))
+
+printf("\n7856 position %s",collection->items[458]->meaning);
+
+//scanf("%s",term);
+//
+// while(strcmp(term,"q"))
+// {
+//   if(!strcmp(term,"s"))
+//   {
+//     scanf("%s",word);
+//     //word[strlen(word)];
+//     result=search(word,&item,&collection);
+//     //  printf("\nReturn position %s",collection->items[position]->meaning);
+//   }
+//   if(!strcmp(term,"i"))
+//   {
+//     scanf("%s",word);
+//     fgets(meaning,BUFFERSIZE,stdin);
+//     result=insert(word,meaning,&item,&collection);
+//   }
+//   if(!strcmp(term,"d"))
+//   {
+//     scanf("%s",word);
+//     result=delete(word,&item,&collection);
+//   }
+//   //printf("AAAAAAAA");
+//
+// scanf("%s",term);
+// }
+//
+//
+
+printf("\n\n\n THE ENDDDDDDDDDDDDDDDDDDDDDDDDDDDD\n\n");
+int i=0;
+do
 {
-  if(!strcmp(term,"s"))
+  free(collection->items[ptr_track[i]]->key);
+  free(collection->items[ptr_track[i]]->meaning);
+  if(!ptr_track[i+1])
   {
-    fgets(word,BUFFERSIZE,stdin);
-    result=search(word,&item,&collection);
+    i=0;
   }
-  if(!strcmp(term,"i"))
+  else
   {
-    scanf("%s",word);
-    fgets(meaning,BUFFERSIZE,stdin);
-    result=insert(word,meaning,&item,&collection);
+    i++;
   }
-  if(!strcmp(term,"d"))
-  {
-    fgets(word,BUFFERSIZE,stdin);
-
-  }
-
-
-}
-
-
+}while(i);
 
 
 
